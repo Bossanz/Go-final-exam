c42d61847a564f3f6331431cdb14016e4bcbc0aa
Bossanz <xbossanz063@gmail.com>
2025-03-26 14:09:44 +0700

    get customer data from database

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"go-final/model"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
-	println("Hello")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(viper.Get("mysql.dsn"))
+	dsn := viper.GetString("mysql.dsn")
+
+	dialactor := mysql.Open(dsn)
+	db, err := gorm.Open(dialactor)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connection successful")
+	customers := []model.Customer{}
+	result := db.Find(&customers)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	fmt.Println(customers)
 }
